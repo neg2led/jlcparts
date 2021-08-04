b1c490f250c658ae9f7fa5d38c8da59b24389c92
yaqwsx <yaqwsx@users.noreply.github.com>
2021-08-04 03:46:17 +0000

    Deploying to gh-pages from @ yaqwsx/jlcparts@c44410640289019470b7b710d3371fb86591b409 🚀

---
+++ precache-manifest.87880d4ab24cc7abba8c3d2da6bbe27f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "feb5ecc54cb7199ca780c729bbc4ff5b",
+    "url": "https://yaqwsx.github.io/jlcparts/index.html"
+  },
+  {
+    "revision": "9fe35bd2597d90b27663",
+    "url": "https://yaqwsx.github.io/jlcparts/static/css/main.1121aab1.chunk.css"
+  },
+  {
+    "revision": "0b1b1a4d8152a49b3ce4",
+    "url": "https://yaqwsx.github.io/jlcparts/static/js/2.33af57ac.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "https://yaqwsx.github.io/jlcparts/static/js/2.33af57ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9fe35bd2597d90b27663",
+    "url": "https://yaqwsx.github.io/jlcparts/static/js/main.f1a59679.chunk.js"
+  },
+  {
+    "revision": "a31c8f175487c705797c",
+    "url": "https://yaqwsx.github.io/jlcparts/static/js/runtime-main.ca322365.js"
+  }
+]);
